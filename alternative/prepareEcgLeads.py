c347b7a3e18cead8bef81e4fde2dc12681d18109
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2022-06-16 16:13:47 -0300

    added limited lenght functionality to reduce dataset size

--- alternative/prepareEcgLeads.py
+++ alternative/prepareEcgLeads.py
@@ -11,28 +11,32 @@ import scipy.io as sio
 dataPath = "alternative/data/"
 scale = 1000
 
+
 def removeZeroPadding(arr: np.ndarray):
     for row in arr:
         i = len(row)-1
-        while (row[-i]==0):
-            i-=1
-        i+=1
-        copyIndex=0
-        for j in range(i,len(row)):
-            row[j]=row[copyIndex]
-            copyIndex+=1
+        while (row[-i] == 0):
+            i -= 1
+        i += 1
+        copyIndex = 0
+        for j in range(i, len(row)):
+            row[j] = row[copyIndex]
+            copyIndex += 1
+
 
 def load_references_normal(folder: str = dataPath) -> Tuple[np.ndarray, np.ndarray]:
     arr = np.load(folder+'ecgLeadsNormal.npy')
     labels = np.load(folder+'ecgLabelsNormal.npy')
 
-    return (arr,labels)
+    return (arr, labels)
+
 
 def load_references_arrhythmia(folder: str = dataPath) -> Tuple[np.ndarray, np.ndarray]:
     arr = np.load(folder+'ecgLeadsArrhythmia.npy')
     labels = np.load(folder+'ecgLabelsArrhythmia.npy')
 
-    return (arr,labels)
+    return (arr, labels)
+
 
 def load_alternative_encoder() -> np.ndarray:
     encoderModel = np.load('encoder.npy', allow_pickle=True)
@@ -41,41 +45,40 @@ def load_alternative_encoder() -> np.ndarray:
 
     for i in range(1, len(encoderModel)):
         encoder[i] = 'A'
-    
+
     return encoder
 
-def matFiles2Array(path: str, label: str):
+
+def matFiles2Array(path: str, label: str, limitedLength=True):
     # loading arrats from csv
     fileNames = [f for f in listdir(path) if isfile(join(path, f))]
-
+    if (limitedLength):
+        fileNames = fileNames[:5000]
 
     data = sio.loadmat(path + fileNames[0])
     ecg_lead = data['sample']
 
-    leads = np.zeros((len(fileNames),len(ecg_lead)))
-    labels=np.full(len(fileNames),label)
+    leads = np.zeros((len(fileNames), len(ecg_lead)))
+    labels = np.full(len(fileNames), label)
 
     counter = 0
     for file in fileNames:
         data = sio.loadmat(path + file)
         ecg_lead = data['sample']
-        leads[counter]=ecg_lead.flatten()
-        counter+=1
+        leads[counter] = ecg_lead.flatten()
+        counter += 1
 
-    return(leads,labels)
+    return(leads, labels)
+
 
 def main():
     norm = matFiles2Array('alternative/normal/', "N")
     arr = matFiles2Array('alternative/arrhythmia/', "A")
 
 
-    # leads= np.append(norm[0],arr[0], axis=0)
-    # labels= np.append(norm[1],arr[1], axis=0)
-
-    np.save(dataPath+'ecgLeadsNormal.npy',norm[0])
-    np.save(dataPath+'ecgLabelsNormal.npy',norm[1])
-    np.save(dataPath+'ecgLeadsArrhythmia.npy',arr[0])
-    np.save(dataPath+'ecgLabelsArrhythmia.npy',arr[1])
-
+    np.save(dataPath+'ecgLeadsNormal.npy', norm[0])
+    np.save(dataPath+'ecgLabelsNormal.npy', norm[1])
+    np.save(dataPath+'ecgLeadsArrhythmia.npy', arr[0])
+    np.save(dataPath+'ecgLabelsArrhythmia.npy', arr[1])
 if __name__ == '__main__':
     main()
