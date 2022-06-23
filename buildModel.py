be9ce4ca08fbc265389cfbc66d2394d7f43c758e
rodrigommorais <52223541+rodrigommorais@users.noreply.github.com>
2022-06-23 17:12:43 +0200

    New features and data processing applyied to buildModel

--- buildModel.py
+++ buildModel.py
@@ -1,27 +1,99 @@
-from statistics import mode
-import pandas as pd
+from wettbewerb import load_references
+from ecgdetectors import Detectors
 import numpy as np
-import xgboost as xgb
+from hrv import HRV
+import pandas as pd
+from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
-
-df = pd.read_csv('data/dropNaN.csv')
-
-x=df[df.columns[:-1]]
-y=df['diagnosis']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,random_state=42)
-
-params = {
-    # Parameters that we are going to tune.
-    'max_depth':8,
-    'eta':.1,
-}
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
+import pickle
+
+# Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name
+ecg_leads,ecg_labels,fs,ecg_names = load_references('./training')                 # Wenn der Ordner mit der Database Training von moodle in einem anderen Ordner liegt, fügen Sie hier den korrigierten Pfad ein 
+detectors = Detectors(fs)                                                         # Initialisierung des QRS-Detektors  
+
+# Initialisierung der Feature-Arrays für Stationary Wavelet Transform Detector Detecto 
+sdnn_swt = np.array([])                                       
+hr_swt = np.array([])                                  
+pNN20_swt = np.array([])                                  
+pNN50_swt = np.array([])      
+fAnalysis_swt = np.array([])   
+
+# Signale verarbeitung
+hrv_class = HRV(fs)
+
+arr = np.zeros((6000,6))
+diagnosis = np.zeros(6000,dtype='str')
+
+for idx, ecg_lead in enumerate(ecg_leads):
+
+    if len(ecg_lead) != 18000:                                  # Length normalization
+        m = 18000 // len(ecg_lead)
+        new_ecg_lead = ecg_lead
+        for i in range(0, m-1):
+            new_ecg_lead = np.append(new_ecg_lead, ecg_lead)
+        r = 18000 - len(new_ecg_lead)
+        new_ecg_lead = np.append(new_ecg_lead, ecg_lead[:r])
+        ecg_leads[idx] = new_ecg_lead
+   
+    r_peaks_swt = detectors.swt_detector(ecg_lead)              # Detektion der QRS-Komplexe mit Stationary Wavelet Transform Detector
+
+#   sdnn_swt = np.std(np.diff(r_peaks_swt)/fs*1000)             # Berechnung der Standardabweichung der Schlag-zu-Schlag Intervalle (SDNN) in Millisekunden - Stationary Wavelet Transform
+    sdnn_swt = hrv_class.SDNN(r_peaks_swt, True)
+    rmssd_swt = hrv_class.RMSSD(r_peaks_swt)
+    sdsd_swt = hrv_class.SDSD(r_peaks_swt)
+    NN20_swt = hrv_class.NN20(r_peaks_swt)
+    NN50_swt = hrv_class.NN50(r_peaks_swt)
+
+#   fAnalysis_swt
+    arr[idx][0] = idx
+    arr[idx][1] = sdnn_swt
+    arr[idx][2] = rmssd_swt
+    arr[idx][3] = sdsd_swt
+    arr[idx][4] = NN20_swt
+    arr[idx][5] = NN50_swt  
+    
+    if ecg_labels[idx] == 'N':
+        diagnosis[idx] = 'N'
+    if ecg_labels[idx] == 'A':
+        diagnosis[idx] = 'A'
+    if ecg_labels[idx] == 'O':
+        diagnosis[idx] = 'O'
+    if ecg_labels[idx] == '~':
+        diagnosis[idx] = '~'
+    if (idx % 100) == 0:
+        print(str(idx) + "\t EKG Signale wurden verarbeitet.")
+
+# Save data with pandas
+df = pd.DataFrame(arr, columns = ['index', 'SDNN', 'RMSSD', 'SDSD', 'NN20', 'NN50'])
+df.drop(['index'],axis=1, inplace = True)
+df.rename(columns = {'level_0':'index'}, inplace = True)
+df['diagnosis'] = diagnosis
+df.dropna(inplace=True)
+
+# Setting input/output
+X=df[df.columns[:-1]]
+y=df['diagnosis']
+
+# Applying oversampling
+oversample = SMOTE()
+X, y = oversample.fit_resample(X, y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+
+# RandomForest model
+clf = RandomForestClassifier(n_estimators=28,max_depth=25,bootstrap=True,min_samples_leaf=1, class_weight="balanced")
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+pickle.dump(clf, open('RF_model.pkl', 'wb'))
+
+# XGB model
 encoder = LabelEncoder()
 y_train = encoder.fit_transform(y_train)
 np.save('encoder.npy', encoder.classes_)
-
-
 model = xgb.XGBClassifier(max_depth=8, learning_rate=0.1, n_estimators=5)
-model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
-model.save_model("model.txt")
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+model.save_model('XGB_model.txt')
