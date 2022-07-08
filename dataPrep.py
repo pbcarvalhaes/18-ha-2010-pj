bb8e32249024f482c8d5bbad757fa3ac20bbab1e
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2022-07-08 19:12:43 -0300

    new format for getting features

--- dataPrep.py
+++ dataPrep.py
@@ -4,6 +4,7 @@ import pandas as pd
 from ecgdetectors import Detectors
 from hrv import HRV
 
+
 def normalizeLead(ecg_lead):
     # normalize each lead to 18000, by copying the lead until 1800 points are reached
     if (len(ecg_lead) != 18000):
@@ -17,12 +18,19 @@ def normalizeLead(ecg_lead):
     else:
         return ecg_lead
 
-def extractFeatures(ecg_leads, fs, leadLengthNormalizer = True):
+
+def extractFeatures(ecg_leads, fs, leadLengthNormalizer=True):
     detectors = Detectors(fs)
     # Signale verarbeitung
     hrv_class = HRV(fs)
 
+    # creating a list of methods to extract each feature
     feautures = [
+        lambda obj: hrv_class.SDNN(obj["r_peaks_swt"], True),
+        lambda obj: hrv_class.RMSSD(obj["r_peaks_swt"]),
+        lambda obj: hrv_class.SDSD(obj["r_peaks_swt"]),
+        lambda obj: hrv_class.NN20(obj["r_peaks_swt"]),
+        lambda obj: hrv_class.NN50(obj["r_peaks_swt"]),
 
     ]
     nfeatures = len(feautures)
@@ -31,31 +39,25 @@ def extractFeatures(ecg_leads, fs, leadLengthNormalizer = True):
     for idx, ecg_lead in enumerate(ecg_leads):
         # normalizing each lead if asked
         if (leadLengthNormalizer):
-            ecg_lead = leadLengthNormalizer(ecg_lead)
+            ecg_lead = normalizeLead(ecg_lead)
 
         # Detektion der QRS-Komplexe mit Stationary Wavelet Transform Detector
         r_peaks_swt = detectors.swt_detector(ecg_lead)
 
-    #   sdnn_swt = np.std(np.diff(r_peaks_swt)/fs*1000)             # Berechnung der Standardabweichung der Schlag-zu-Schlag Intervalle (SDNN) in Millisekunden - Stationary Wavelet Transform
-        sdnn_swt = hrv_class.SDNN(r_peaks_swt, True)
-        rmssd_swt = hrv_class.RMSSD(r_peaks_swt)
-        sdsd_swt = hrv_class.SDSD(r_peaks_swt)
-        NN20_swt = hrv_class.NN20(r_peaks_swt)
-        NN50_swt = hrv_class.NN50(r_peaks_swt)
-
+        # calling each feature method and sending every parameter to extract desired feature
         for i in range(nfeatures):
-            arr[idx][i] = feautures(
-                ecg_lead,
-                r_peaks_swt
-                )
+            leadElements = {
+                "ecg_lead": ecg_lead,
+                "r_peaks_swt": r_peaks_swt,
+            }
+            arr[idx][i] = feautures[i](leadElements)
 
         if (idx % 1000) == 0:
             print(str(idx) + "\t EKG Signale wurden verarbeitet.")
 
     # Save data with pandas
-    df = pd.DataFrame(arr, columns=['index', 'SDNN', 'RMSSD', 'SDSD',
-                      'NN20', 'NN50', 'min moduled', 'max moduled', 'mean moduled'])
+    df = pd.DataFrame(arr)
     df.drop(['index'], axis=1, inplace=True)
     df.rename(columns={'level_0': 'index'}, inplace=True)
-    
+
     return df
